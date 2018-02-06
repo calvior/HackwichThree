10c0f7ac3501963e75f14d8b449a3fdf0f57ba49
calvior <calvior@hawaii.edu>
2018-02-05 21:42:57 -1000

    hackwichThree complete

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstTabLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    self.firstTabLabel.text = "About"
     }
 
     override func didReceiveMemoryWarning() {
---
+++ secondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  secondViewController.swift
+//  HackwichThree
+//
+//  Created by Caitlin Alvior on 2/5/18.
+//  Copyright © 2018 Caitlin Alvior. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBOutlet var secondTabLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    self.secondTabLabel.text = "Tab Two"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
